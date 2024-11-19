ea4f3fa69af89382bd395cbdc647b4fb1543d0e3
UriTabic <tabicuri123@gmail.com>
2024-11-19 23:32:19 +0200

    Now staged

--- Peer/src/Utils/FileUtils/FileUtils.cpp
+++ Peer/src/Utils/FileUtils/FileUtils.cpp
@@ -1,11 +1,10 @@
 #include "FileUtils.h"
 
-
-std::vector<uint8_t> FileUtils::readFileToVector(const std::string& filePath)
+std::vector<uint8_t> FileUtils::readFileToVector(const std::string &filePath)
 {
     std::ifstream file(filePath, std::ios::in | std::ios::binary);
 
-    if (!file) 
+    if (!file)
     {
         throw std::runtime_error("Could not open the file: " + filePath);
     }
@@ -17,7 +16,7 @@ std::vector<uint8_t> FileUtils::readFileToVector(const std::string& filePath)
 
     // Resize vector to hold the file data and read the file contents
     std::vector<uint8_t> content(fileSize);
-    if (file.read(reinterpret_cast<char*>(content.data()), fileSize)) 
+    if (file.read(reinterpret_cast<char *>(content.data()), fileSize))
     {
         return content;
     }
@@ -25,4 +24,15 @@ std::vector<uint8_t> FileUtils::readFileToVector(const std::string& filePath)
     {
         throw std::runtime_error("Error reading the file: " + filePath);
     }
+}
+
+void FileUtils::writeVectorToFile(const std::vector<uint8_t> &data, const std::string &filePath)
+{
+    std::ofstream outFile(filePath, std::ios::out | std::ios::binary | std::ios::trunc);
+    if (!outFile)
+    {
+        throw std::ios_base::failure("Failed to open file for writing");
+    }
+    outFile.write(reinterpret_cast<const char *>(data.data()), data.size());
+    outFile.close();
 }
--- Peer/src/Utils/FileUtils/FileUtils.h
+++ Peer/src/Utils/FileUtils/FileUtils.h
@@ -7,9 +7,7 @@
 
 class FileUtils
 {
-    public:
-
-    static std::vector<uint8_t> readFileToVector(const std::string& filePath); // Function receives a filePath and returns a file contents (also valid for non txt files)
-
-
+public:
+    static std::vector<uint8_t> readFileToVector(const std::string &filePath);
+    static void writeVectorToFile(const std::vector<uint8_t> &data, const std::string &filePath);
 };
