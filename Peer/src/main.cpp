07910c997e5d711827f1c46bf19e2ce0871ab7d9
UriTabic <tabicuri123@gmail.com>
2024-11-21 00:35:47 +0200

    Added example

--- Peer/src/main.cpp
+++ Peer/src/main.cpp
@@ -6,7 +6,7 @@ int main()
     // auto a = MetaDataFile::createMetaData("/home/user/Desktop/Maghshimim/Bit Torrent Uri + Nina/Gitabic/Peer/test.txt", "12345678", Address(), "Uri");
     // auto b = a.serialize();
     // Utils::FileUtils::writeVectorToFile(b, "/home/user/Desktop/Maghshimim/Bit Torrent Uri + Nina/Gitabic/Peer/test.gitabic");
-    auto a = Utils::FileUtils::readFileToVector("/home/user/Desktop/Maghshimim/Bit Torrent Uri + Nina/Gitabic/Peer/test.gitabic"); // Need to create 300000001 'a' in test.txt
+    auto a = Utils::FileUtils::readFileToVector("/home/user/Desktop/Maghshimim/Bit Torrent Uri + Nina/Gitabic/Peer/test.gitabic"); // Need to create 300000001 'a' in test.txt : python3 -c 'print("a" * (99999999 + 1)*3)' > test.txt
     MetaDataFile metaDataFile(a);
     SHA256::printHashAsString(metaDataFile.getPartsHashes()[0]);
     SHA256::printHashAsString(SHA256::toHashSha256(Utils::FileUtils::readFileChunk("/home/user/Desktop/Maghshimim/Bit Torrent Uri + Nina/Gitabic/Peer/test.txt", 0, Utils::FileSplitter::pieceSize(metaDataFile.getFileSize()))));
