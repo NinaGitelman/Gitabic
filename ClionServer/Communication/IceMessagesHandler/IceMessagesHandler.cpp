4cd39464389231a4b1fbd52a709dba978a8bb794
UriTabic <tabicuri123@gmail.com>
2024-12-07 23:52:20 +0200

    Started copying server

---
+++ ClionServer/Communication/IceMessagesHandler/IceMessagesHandler.cpp
@@ -0,0 +1,31 @@
+#include "IceMessagesHandler.h"
+
+// Define static members
+std::unique_ptr<IceMessagesHandler> IceMessagesHandler::instance = nullptr;
+std::once_flag IceMessagesHandler::initInstanceFlag;
+
+// Access the single instance
+IceMessagesHandler &IceMessagesHandler::getInstance()
+{
+    std::call_once(initInstanceFlag, []()
+                   { instance.reset(new IceMessagesHandler()); });
+    return *instance;
+}
+
+ResultMessage IceMessagesHandler::handle(const ClientRequestGetUserICEInfo &request)
+{
+    ResultMessage res;
+    res.id = request.RequestedUserId;
+    res.msg = std::make_shared<ServerRequestAuthorizeICEConnection>(request.iceCandidateInfo, waitIds);
+    waitsForResponse[waitIds++] = request.from;
+    return res;
+}
+
+ResultMessage IceMessagesHandler::handle(const ClientResponseAuthorizedICEConnection &request)
+{
+    ResultMessage res;
+    res.id = waitsForResponse[request.requestId];
+    waitsForResponse.erase(request.requestId);
+    res.msg = std::make_shared<ServerResponseUserAuthorizedICEData>(request.iceCandidateInfo);
+    return res;
+}
---
+++ ClionServer/Utils/SerializeDeserializeUtils/SerializeDeserializeUtils.h
@@ -0,0 +1,33 @@
+
+#pragma once
+#include <iostream>
+#include <vector>
+#include <array>
+#include <stdio.h>
+
+using std::vector;
+
+class SerializeDeserializeUtils
+{
+public:
+    /// @brief Function to serailize a short (uint16_t) into a vector to then copy into the serialized vector
+    /// @param buffer
+    /// @param num the unit16_t to serialize
+    /// @return the serialized vector (size 2)
+    static void serializeUint16IntoVector(vector<uint8_t> &buffer, uint16_t num);
+
+
+    static void addToFront(vector<uint8_t> &to, const vector<uint8_t> &from);
+    static void addToEnd(vector<uint8_t> &to, const vector<uint8_t> &from);
+
+    template <size_t N>
+    static void addToFront(std::vector<uint8_t> &to, const std::array<uint8_t, N> &from)
+    {
+        to.insert(to.begin(), from.begin(), from.end());
+    }
+    template <size_t N>
+    static void addToEnd(std::vector<uint8_t> &to, const std::array<uint8_t, N> &from)
+    {
+        to.insert(to.end(), from.begin(), from.end());
+    }
+};
