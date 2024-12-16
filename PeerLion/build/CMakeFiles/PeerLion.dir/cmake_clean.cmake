da228fb15aca498e2f5bd8ed4b085f1686db0c74
ninavwx <ninavwx@gmail.com>
2024-12-16 11:10:33 +0200

    Changed cmake min req to make on my terminal (to run 2 peers + using atomic bool instead of bool and mutex

---
+++ PeerLion/build/CMakeFiles/PeerLion.dir/cmake_clean.cmake
@@ -0,0 +1,37 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PeerLion.dir/Encryptions/AES/AESHandler.cpp.o"
+  "CMakeFiles/PeerLion.dir/Encryptions/AES/AESHandler.cpp.o.d"
+  "CMakeFiles/PeerLion.dir/Encryptions/SHA256/sha256.cpp.o"
+  "CMakeFiles/PeerLion.dir/Encryptions/SHA256/sha256.cpp.o.d"
+  "CMakeFiles/PeerLion.dir/ICEConnection/ICEConnection.cpp.o"
+  "CMakeFiles/PeerLion.dir/ICEConnection/ICEConnection.cpp.o.d"
+  "CMakeFiles/PeerLion.dir/NetworkUnit/PseudoTCP/TCPConnection.cpp.o"
+  "CMakeFiles/PeerLion.dir/NetworkUnit/PseudoTCP/TCPConnection.cpp.o.d"
+  "CMakeFiles/PeerLion.dir/NetworkUnit/ServerComm/DataRepublish/DataRepublish.cpp.o"
+  "CMakeFiles/PeerLion.dir/NetworkUnit/ServerComm/DataRepublish/DataRepublish.cpp.o.d"
+  "CMakeFiles/PeerLion.dir/NetworkUnit/ServerComm/TCPSocket/TCPSocket.cpp.o"
+  "CMakeFiles/PeerLion.dir/NetworkUnit/ServerComm/TCPSocket/TCPSocket.cpp.o.d"
+  "CMakeFiles/PeerLion.dir/NetworkUnit/SocketHandler/SocketHandler.cpp.o"
+  "CMakeFiles/PeerLion.dir/NetworkUnit/SocketHandler/SocketHandler.cpp.o.d"
+  "CMakeFiles/PeerLion.dir/Utils/DowndloadProgress/DownloadProgress.cpp.o"
+  "CMakeFiles/PeerLion.dir/Utils/DowndloadProgress/DownloadProgress.cpp.o.d"
+  "CMakeFiles/PeerLion.dir/Utils/FileUtils/FileUtils.cpp.o"
+  "CMakeFiles/PeerLion.dir/Utils/FileUtils/FileUtils.cpp.o.d"
+  "CMakeFiles/PeerLion.dir/Utils/MetaDataFile/MetaDataFile.cpp.o"
+  "CMakeFiles/PeerLion.dir/Utils/MetaDataFile/MetaDataFile.cpp.o.d"
+  "CMakeFiles/PeerLion.dir/Utils/SerializeDeserializeUtils.cpp.o"
+  "CMakeFiles/PeerLion.dir/Utils/SerializeDeserializeUtils.cpp.o.d"
+  "CMakeFiles/PeerLion.dir/Utils/ThreadSafeCout.cpp.o"
+  "CMakeFiles/PeerLion.dir/Utils/ThreadSafeCout.cpp.o.d"
+  "CMakeFiles/PeerLion.dir/Utils/VectorUint8Utils.cpp.o"
+  "CMakeFiles/PeerLion.dir/Utils/VectorUint8Utils.cpp.o.d"
+  "CMakeFiles/PeerLion.dir/main.cpp.o"
+  "CMakeFiles/PeerLion.dir/main.cpp.o.d"
+  "PeerLion"
+  "PeerLion.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PeerLion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
