f303ded21ea07bb631ef1b71042519543cd8d0cb
ninavwx <ninavwx@gmail.com>
2024-12-18 16:20:54 +0200

    done peer torrent struct

--- PeerLion/Torrent/ConnectionManager/ConnectionManager.h
+++ PeerLion/Torrent/ConnectionManager/ConnectionManager.h
@@ -4,18 +4,24 @@
 #pragma once
 
 #include "../../ICEConnection/ICEConnection.h"
-#include "../../NetworkUnit/SocketHandler/SocketHandler.h"
 
 
 // technically the same as addres but im leaving it under a different name in case we want to add more things...
 struct PeerTorrent
 {
-    Address address;
+    ID id;
 
 
     bool operator==(const PeerTorrent &other) const
     {
-        return this->address == other.address;
+      bool isEqual = false;
+      // this works as the arrays of the id are of equal size
+      if(this->id.size() == other.id.size())
+      {
+          isEqual =  this->id == other.id;
+      }
+
+      return isEqual;
     }
 
 };
