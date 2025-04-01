04d18d9fb975fe003b5b7dad9e411187f413d2ae
nina <ninavwx@gmail.com>
2025-04-01 23:31:52 +0300

    changed amount of peers i share with

--- PeerLion/Torrent/PeerManager/PeerManager.h
+++ PeerLion/Torrent/PeerManager/PeerManager.h
@@ -10,7 +10,7 @@
 #include <vector>
 #include "../PeersConnectionManager/PeersConnectionManager.h"
 
-#define MAX_PEERS 20
+#define MAX_PEERS 12
 
 struct PeerState {
 	bool amChoking, amInterested, peerChoking, peerInterested;
